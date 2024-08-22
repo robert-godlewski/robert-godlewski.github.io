41d6263c303d7282d30dc6534b80d407f3631732
Robert Godlewski <88469054+robert-godlewski@users.noreply.github.com>
2024-08-22 14:40:23 -0700

    Redesign Portfolio (#3) (#5)
    * adjusted the summary
    
    * started fixing software services file
    
    * doctored up the services and summary into one file
    
    * adjusted the color scheme
    
    * added email to software services description

--- src/components/AudioView.js
+++ src/components/AudioView.js
@@ -16,29 +16,29 @@ const AudioView = () => {
     return (
         <div>
             <NavBar/>
-            <div className='container border col col-11 my-2'>
+            <div className='container border bg-dark-subtle text-dark col col-11 my-2'>
                 <img src={logo} alt="Comet of Dreams Logo" height="100px" className='my-2'></img>
                 <RenderMarkup fileName={`${root_path}audio_services.md`}/>
             </div>
             <div className='container'>
                 <h2>Audio Production Projects</h2>
                 <div className='row'>
-                    <div className='border col col-lg-5 col-md-10 col-11 my-2 mx-xxl-5 mx-lg-4 mx-md-2 mx-1'>
+                    <div className='border bg-danger-subtle text-dark col col-lg-5 col-11 my-2 mx-xxl-5 mx-lg-4 mx-md-2 mx-1'>
                         <RenderMarkup fileName={`${root_path}cod_crusader.md`}/>
                     </div>
-                    <div className='border col col-lg-5 col-md-10 col-11 my-2 mx-xxl-5 mx-lg-4 mx-md-2 mx-1'>
+                    <div className='border bg-warning-subtle text-dark col col-lg-5 col-11 my-2 mx-xxl-5 mx-lg-4 mx-md-2 mx-1'>
                         <RenderMarkup fileName={`${root_path}madrigals.md`}/>
                     </div>
-                    <div className='border col col-lg-5 col-md-10 col-11 my-2 mx-xxl-5 mx-lg-4 mx-md-2 mx-1'>
+                    <div className='border bg-success-subtle text-dark col col-lg-5 col-11 my-2 mx-xxl-5 mx-lg-4 mx-md-2 mx-1'>
                         <RenderMarkup fileName={`${root_path}cod_voyage.md`}/>
                     </div>
-                    <div className='border col col-lg-5 col-md-10 col-11 my-2 mx-xxl-5 mx-lg-4 mx-md-2 mx-1'>
+                    <div className='border bg-primary-subtle text-dark col col-lg-5 col-11 my-2 mx-xxl-5 mx-lg-4 mx-md-2 mx-1'>
                         <RenderMarkup fileName={`${root_path}cover_folsom_prison_blues.md`}/>
                     </div>
-                    <div className='border col col-lg-5 col-md-10 col-11 my-2 mx-xxl-5 mx-lg-4 mx-md-2 mx-1'>
+                    <div className='border bg-info text-dark col col-lg-5 col-11 my-2 mx-xxl-5 mx-lg-4 mx-md-2 mx-1'>
                         <RenderMarkup fileName={`${root_path}sbd_up_to_chance.md`}/>
                     </div>
-                    <div className='border col col-lg-5 col-md-10 col-11 my-2 mx-xxl-5 mx-lg-4 mx-md-2 mx-1'>
+                    <div className='border bg-secondary col col-lg-5 col-11 my-2 mx-xxl-5 mx-lg-4 mx-md-2 mx-1'>
                         <RenderMarkup fileName={`${root_path}beyond_jagged.md`}/>
                     </div>
                 </div>
